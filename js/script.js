822a6f73e05f149abb7dec21345af5fcae126f98
oken <wwwtarouwww1107@gmail.com>
2025-04-29 20:43:12 +0900

    web-service_LP

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// スムーススクロール ドロワーメニュー
+jQuery('#js-drawer-menu a[href^="#"]').on("click", function (e) {
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-menu").slideUp(400).removeClass("is-checked");
+});
+
+// ドロワーメニュー呼び出し
+const $trigger = jQuery("#js-drawer-icon, #js-open-txt");
+const $menu = jQuery("#js-drawer-menu");
+
+$trigger.on("click", function(e) {
+  e.preventDefault();
+  $trigger.toggleClass("is-checked");
+  $menu.stop(true).slideToggle(300).toggleClass("is-checked");
+});
